67156c1f1cc27664268ee5a1d5ee56ea7f52ae21
Tyler Eich <tyler2eich@gmail.com>
2014-02-18 16:59:06 -0500

    First beta of website

---
+++ script.js
@@ -0,0 +1,5 @@
+function ListController($scope) {
+    // https://raw.github.com/TylerEich/Alfred-Extras/readme/Source/Colors/icon.png
+    $scope.baseUrl = 'https://raw.github.com/TylerEich/Alfred-Extras/readme/Source/';
+    $scope.paths = ['Colors', 'Favorites', 'Versions', 'Wolfram-Alpha'];
+}
