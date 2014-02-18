a6425f92de49b29c93a99d55cabbcf74926c2a5e
Tyler Eich <tyler2eich@gmail.com>
2014-02-18 17:27:14 -0500

    Downloads linked

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function ListController($scope) {
     // https://raw.github.com/TylerEich/Alfred-Extras/readme/Source/Colors/icon.png
     $scope.baseUrl = 'https://raw.github.com/TylerEich/Alfred-Extras/readme/Source/';
-    $scope.paths = ['Colors', 'Favorites', 'Versions', 'Wolfram-Alpha'];
+    $scope.items = [{
+        name: 'Colors',
+        path: 'Colors',
+        packalUrl: 'colors'
+    }, {
+        name: 'Versions',
+        path: 'Versions',
+        packalUrl: 'versions'
+    }, {
+        name: 'Wolfram|Alpha',
+        path: 'Wolfram-Alpha',
+        packalUrl: 'wolframalpha'
+    }];
 }
