940c737b9fa792f0fe2e49d3e3dc7e39539d0b24
Tyler Eich <tyler2eich@gmail.com>
2014-02-19 21:59:48 -0500

    Updated webpage layout, added extended descriptions

--- Source/Favorites/README.md
+++ Source/Favorites/README.md
@@ -1,5 +1,5 @@
-# Colors
+# Favorites
 
-#### Generate, convert, and preview color notations
+#### Save your favorite files and folders for quick access
 
-Colors is a tool for programmers and designers. It processes any supported color input format, generates a preview of that color, and converts it to other supported formats.
+Favorites Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at magna non mi tincidunt viverra. Mauris sit amet nunc nec odio adipiscing eleifend at eu lectus. Nullam ipsum magna, aliquet in cursus nec, consequat tempor metus. Phasellus porttitor urna a magna commodo rhoncus aliquet libero faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et lobortis quam. Integer gravida nisi vitae diam varius molestie faucibus arcu pharetra. Integer id elit felis, et fringilla nunc. Suspendisse vehicula, ligula non luctus varius, purus sem tincidunt leo, ac fermentum tortor magna sed nulla. Cras at metus mi, at porta erat. Proin blandit porta venenatis.
--- script.js
+++ script.js
@@ -1,17 +1,62 @@
+var app = angular.module('alfred-extras', ['ui.router']);
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('root', {
+        url: '',
+        templateUrl: 'website/template/list.html',
+        controller: ['$scope', ListController]
+    })
+    .state('detail', {
+        url: '/:choice',
+        templateUrl: 'website/template/detail.html',
+        controller: ['$scope', '$stateParams', '$sce', DetailController]
+    });
+}]);
+
+function ItemController($scope, $http) {
+    $scope.items = {};
+    
+    function name(response) {
+        var start = response.indexOf('\n# ') + 3;
+        var end = response.indexOf('\n', start);
+        
+        return response.substring(start, end);
+    }
+    
+    $http.get('https://api.github.com/repos/TylerEich/Alfred-Extras/' + 'contents/Source?access_token=001a02138839885be592af28b8b0f615357c2cef&ref=gh-pages')
+    .success(function(data) {
+        for (var i = 0; i < data.length; i++) {            
+            (function(i, data) {
+                $http.get('Source/' + data[i].name + '/README.md')
+                .success(function(response) {
+                    $scope.items[name(response)] = response;
+                    // $scope.items.push({
+                    //     name: name(response),
+                    //     readme: response
+                    // });
+                });
+            })(i, data);
+        }
+    });
+}
+
 function ListController($scope) {
-    // https://raw.github.com/TylerEich/Alfred-Extras/readme/Source/Colors/icon.png
-    $scope.baseUrl = 'https://raw.github.com/TylerEich/Alfred-Extras/readme/Source/';
-    $scope.items = [{
-        name: 'Colors',
-        path: 'Colors',
-        packalUrl: 'colors'
-    }, {
-        name: 'Versions',
-        path: 'Versions',
-        packalUrl: 'versions'
-    }, {
-        name: 'Wolfram|Alpha',
-        path: 'Wolfram-Alpha',
-        packalUrl: 'wolframalpha'
-    }];
+    $scope.summary = function(readme) {
+        var start = readme.indexOf('\n#### ') + 6;
+        var end = readme.indexOf('\n', start);
+        
+        return readme.substring(start, end);
+    };
+}
+
+function DetailController($scope, $stateParams, $sce) {
+    $scope.choice = $stateParams.choice;
+    
+    $scope.readme = function() {
+        var readme = $scope.items[$scope.choice];
+                
+        if (!readme) return;
+        console.log(marked(readme));
+        return $sce.trustAsHtml(marked(readme));
+    };
 }
